ae39a8ba557c9c4c90368bac5f190b47031b993c
Milica1234567 <75258306+Milica1234567@users.noreply.github.com>
2023-04-21 22:58:05 +0200

    login

---
+++ login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prijava</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    
+</head>
+<body>
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <!-- Brand -->
+    <a class="navbar-brand" href="#"><img src="./img/logo34.png" alt="" class="logo"></a>
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="login.php">dr Jelena Todic</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">dr Jovana Radenovic</a>
+        </li>
+        </ul>
+    </div>
+</nav>
+<div class="container">
+    <div class="login-box">
+    <div class="row">
+        <div class="col-md-6">
+            <h2>LOGIN</h2>
+            <form action="validation.php" method="post">
+                <div class="form-group">
+                    <label for="">Username</label>
+                    <input type="text" name="doktor" class="form-control" required>
+                    <div class="form-group">
+                    <label for="">Password</label>
+                    <input type="password" name="password" class="form-control" required>
+                </div>
+                <button type="submit" class="loginBtn">LOGIN</button>
+                </div>
+            </form>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
